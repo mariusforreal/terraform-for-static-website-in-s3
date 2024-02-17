18277fe72a9d0c609640b3a0d6c8f1ab0f681401
Marius Songwa <mariusforreal@gmail.com>
2024-02-17 10:54:30 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-1"
+}
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     = "DevOps-Key"     #change key name here
+}
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     = "t2.medium"
+}
+variable "ami_id" {
+  description = "AMI for Ubuntu Ec2 instance"
+  default     = "ami-0c7217cdde317cfec"
+}
+variable "bucketname" {
+  description = "The name of the S3 bucket to create"
+  type        = string
+  default     = "devops-00237-00237"  #change Bucket name also
+}
